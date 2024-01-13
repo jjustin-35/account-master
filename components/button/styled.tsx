0ac916102352958ca4728f8c78247e353c8b4ebf
jjustin-chen <justin.chen@kdanmobile.com>
2024-01-13 23:27:35 +0800

    update: button

--- components/button/styled.tsx
+++ components/button/styled.tsx
@@ -2,12 +2,12 @@ import styled, { RuleSet, css } from 'styled-components';
 import { btnThemes, btnSizes, btnRounds } from '@/constants/uiTypes';
 import colors from '@/constants/styles/colors';
 
-const themes: Record<keyof typeof btnThemes, RuleSet<object>> = {
-  primary: css`
+const themes: Record<btnThemes, RuleSet<object>> = {
+  [btnThemes.primary]: css`
     background-color: ${colors.primary};
     color: #ffffff;
   `,
-  secondary: css`
+  [btnThemes.secondary]: css`
     background-color: ${colors.secondary};
     border: 1px solid ${colors.secondary};
     color: #000000;
@@ -17,33 +17,33 @@ const themes: Record<keyof typeof btnThemes, RuleSet<object>> = {
       color: ${colors.secondary};
     }
   `,
-  primaryOutline: css`
+  [btnThemes.primaryOutline]: css`
     background-color: transparent;
     border: 1px solid ${colors.primary};
     color: ${colors.primary};
   `,
 };
 
-const sizes: Record<keyof typeof btnSizes, RuleSet<object>> = {
-  small: css`
+const sizes: Record<btnSizes, RuleSet<object>> = {
+  [btnSizes.small]: css`
     padding: 8px 16px;
     font-size: 14px;
   `,
-  medium: css`
+  [btnSizes.medium]: css`
     padding: 10px 20px;
     font-size: 16px;
   `,
-  large: css`
+  [btnSizes.large]: css`
     padding: 1px 24px;
     font-size: 20px;
   `,
 };
 
-const borderRadius: Record<keyof typeof btnRounds, RuleSet<object>> = {
-  normal: css`
+const borderRadius: Record<btnRounds, RuleSet<object>> = {
+  [btnRounds.normal]: css`
     border-radius: 4px;
   `,
-  pill: css`
+  [btnRounds.pill]: css`
     border-radius: 9999px;
   `,
 };
@@ -60,7 +60,7 @@ export const Wrapper = styled.a<{
   transition: all 0.3s;
   padding: 8px 16px;
 
-  ${({ $btnTheme }) => themes[$btnTheme || 'primary']};
-  ${({ $size }) => sizes[$size || 'medium']};
-  ${({ $round }) => borderRadius[$round || 'normal']};
+  ${({ $btnTheme }) => themes[$btnTheme || btnThemes.primary]};
+  ${({ $size }) => sizes[$size || btnSizes.medium]};
+  ${({ $round }) => borderRadius[$round || btnRounds.normal]};
 `;
