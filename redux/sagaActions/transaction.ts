e7a58b6037614e5854f043a23932018cbb340f81
jjustin-chen <justin.chen@kdanmobile.com>
2024-02-15 01:06:20 +0800

    update: getTransactionList

--- redux/sagaActions/transaction.ts
+++ redux/sagaActions/transaction.ts
@@ -4,3 +4,7 @@ import { createAction } from '@reduxjs/toolkit';
 export const createTransaction = createAction<DailyTransactionType>(
   'counting/createTransaction',
 );
+
+export const getTransactionList = createAction<{ month: number }>(
+  'counting/getTransactionList',
+);
