659b295d8e1e28a5d464f066f953dfbb3b458757
jjustin-35 <jjusttin11@gmail.com>
2024-02-27 21:32:06 +0800

    update: config

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,7 +1,6 @@
 import { redirect } from 'next/navigation';
 
 import mobileDetect from '@/helpers/mobileDetect';
-
 import IntroductionContent from '@/components/onboarding';
 
 const Onboarding = () => {
--- next.config.js
+++ next.config.js
@@ -1,19 +1,37 @@
 /** @type {import('next').NextConfig} */
 const secureEnv = require('secure-env');
+const webpack = require('webpack');
 
 global.env = secureEnv({secret: `blue-bird${process.env.PASSWORD}`});
+const isDev = process.env.NODE_ENV !== 'production';
+
+const env = isDev ? global.env : process.env;
+const envObj = Object.keys(env).reduce((acc, key) => {
+  return {
+    ...acc,
+    [key]: JSON.stringify(env[key]),
+  }
+}, {});
 
 const nextConfig = {
   publicRuntimeConfig: {
     NODE_ENV: process.env.NODE_ENV,
     ENV: process.env.ENV,
   },
-  experimental: { serverComponentsExternalPackages: ['mongoose'] },
   compiler: {
     styledComponents: { displayName: true, ssr: true },
   },
   webpack: (config) => {
     config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env': {
+          ...envObj,
+          NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+          ENV: JSON.stringify(process.env.ENV),
+        },
+      })
+    );
 
     return config;
   },
